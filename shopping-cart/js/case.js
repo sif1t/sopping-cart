8abed13364b6381cc7974a95b5ed842431d5f780
arifeensifat01 <arifeen.sifat01@gmail.com>
2024-01-09 19:24:23 +0600

    Add .vscode settings and shopping cart files

---
+++ shopping-cart/js/case.js
@@ -0,0 +1,7 @@
+ document.getElementById("btn-case-plus").addEventListener("click", function(){
+    const caseNumberField = document.getElementById("case-number-field");
+    const  caseNumberString = caseNumberField.value;
+    const caseNumber = parseInt(caseNumberString);
+ });
+
+ 
