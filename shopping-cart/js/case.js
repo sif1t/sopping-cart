8227260f6e8dac1cb73426029ccf02b069349d70
arifeensifat01 <arifeen.sifat01@gmail.com>
2024-01-09 20:56:16 +0600

    Refactor case.js to update case total price dynamically

--- shopping-cart/js/case.js
+++ shopping-cart/js/case.js
@@ -24,17 +24,24 @@ function updateCaseNumber(isIncraese){
 }
 
 
-
- document.getElementById("btn-case-plus").addEventListener("click", function(){
-    const newCaseNumber =  updateCaseNumber(true);
-   
+  function updateCaseTotalPrice (newCaseNumber){
     const caseTotalPrice = newCaseNumber * 59;
     const caseTotalElement = document.getElementById("case-totla");
     caseTotalElement.innerText = caseTotalPrice;
+  }
+
+ document.getElementById("btn-case-plus").addEventListener("click", function(){
+    const newCaseNumber =  updateCaseNumber(true);
+
+    updateCaseTotalPrice(newCaseNumber);
+   
+    
  });
 
     document.getElementById("btn-case-minus").addEventListener("click", function(){
-         updateCaseNumber(false);
+         const newCaseNumber = updateCaseNumber(false);
+          
+         updateCaseTotalPrice(newCaseNumber);
     });
 
 
