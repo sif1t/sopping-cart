73e06c3886f8bd8c992aa41a09cf8b9edde4008a
arifeensifat01 <arifeen.sifat01@gmail.com>
2024-01-09 20:32:03 +0600

    Refactor case number update logic in case.js

--- shopping-cart/js/case.js
+++ shopping-cart/js/case.js
@@ -6,20 +6,34 @@
 5. set the value to the case number field
 */ 
  
- document.getElementById("btn-case-plus").addEventListener("click", function(){
+function updateCaseNumber(isIncraese){
     const caseNumberField = document.getElementById("case-number-field");
     const  caseNumberString = caseNumberField.value;
     const previousCaseNumber = parseInt(caseNumberString);
-    const newCaseNumber = previousCaseNumber + 1;
+    
+    let newCaseNumber;
+    if(isIncraese === true){
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
+
     caseNumberField.value = newCaseNumber;
+    return newCaseNumber;
+}
+
+
+
+ document.getElementById("btn-case-plus").addEventListener("click", function(){
+    const newCaseNumber =  updateCaseNumber(true);
+    const NewCasePrice = 59;
+    const caseTotal = newCaseNumber * NewCasePrice;
  });
 
     document.getElementById("btn-case-minus").addEventListener("click", function(){
-    const caseNumberField = document.getElementById("case-number-field");
-    const  caseNumberString = caseNumberField.value;
-    const previousCaseNumber = parseInt(caseNumberString);
-    const newCaseNumber = previousCaseNumber - 1;
-    caseNumberField.value = newCaseNumber;
+         updateCaseNumber(false);
     });
+
 
  
