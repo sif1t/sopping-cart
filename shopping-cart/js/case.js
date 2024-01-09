ffe0d67e72d9323b403b6afcc89318bb1c6938ac
arifeensifat01 <arifeen.sifat01@gmail.com>
2024-01-09 19:34:23 +0600

    Add event listener to plus button and update case number field

--- shopping-cart/js/case.js
+++ shopping-cart/js/case.js
@@ -1,7 +1,17 @@
+/*
+1. add event listener to plus button
+2.get the value inside the case number field (input field)
+3. convert the number to an integer
+4. celculate the new case number
+5. set the value to the case number field
+*/ 
+ 
  document.getElementById("btn-case-plus").addEventListener("click", function(){
     const caseNumberField = document.getElementById("case-number-field");
     const  caseNumberString = caseNumberField.value;
-    const caseNumber = parseInt(caseNumberString);
+    const previousCaseNumber = parseInt(caseNumberString);
+    const newCaseNumber = previousCaseNumber + 1;
+    caseNumberField.value = newCaseNumber;
  });
 
  
