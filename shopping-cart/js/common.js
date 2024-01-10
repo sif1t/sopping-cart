04ce31bcc6e435f8073391bcec46044a7e5f100f
arifeensifat01 <arifeen.sifat01@gmail.com>
2024-01-10 17:17:25 +0600

    Refactor common.js to improve readability and add functionality

--- shopping-cart/js/common.js
+++ shopping-cart/js/common.js
@@ -5,12 +5,28 @@ function getTextElementValueById(elementId) {
     return CurrentPhoneTotal;
 }
 
+function setTextElementValueById ( elementId, value){
+    const subTotalElement = document.getElementById(elementId);
+    subTotalElement.innerText = value;
+
+}
+
 function calculatSubTotal(){
     // calculate total price
     const CurrentPhoneTotal = getTextElementValueById('phone-total');
     const CurrentCaseTotal = getTextElementValueById('case-totla');
 
     const CurrentSubTotal = CurrentPhoneTotal + CurrentCaseTotal;
-    const subTotalElement = document.getElementById('sub-total');
-    subTotalElement.innerText = CurrentSubTotal;
+    setTextElementValueById('sub-total', CurrentSubTotal);
+    
+
+    // calculate tax
+    const taxAmountString = (CurrentSubTotal * 0.1).toFixed(2);
+    const taxAmount = parseFloat(taxAmountString)
+    setTextElementValueById('tax-amount', taxAmount);
+
+    // calculate total price
+    const finalAmount = CurrentSubTotal + taxAmount;
+    setTextElementValueById('fianl-total', finalAmount);
+    
 }
